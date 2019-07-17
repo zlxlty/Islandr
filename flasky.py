fadff5318e44253cc955c01dec116a3147b8e3a8
dingding0606 <yfwu17@uwcchina.org>
2019-07-16 23:27:03 -0500

    full image

--- flasky.py
+++ flasky.py
@@ -19,6 +19,12 @@ def test():
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
 
+# 这里应该重构一下，不知道放哪好
 @app.template_filter() # Jinja2 custom filter
 def str_to_dic(str):
     return json.loads(str)
+
+@app.template_filter() # Jinja2 custom filter: remove ".jpg" part or any extension of a file
+def remove_ext(file_name):
+    components = file_name.split(".")
+    return components[0]
