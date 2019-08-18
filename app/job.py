4db0047e820065a2f1b3e81462221f4e299e7034
MultyXu <982185256@qq.com>
2019-08-18 20:22:50 +0800

    bulletin & reminder modified

--- app/job.py
+++ app/job.py
@@ -9,16 +9,6 @@ from flask import current_app, render_template
 from . import scheduler
 from .email import send_simple_email, reminder_email, bulletin_email
 
-#testing function 
-def add_job():
-    scheduler.add_job('job1', job_1, trigger='interval', seconds=2)
-
-def sending_emails():
-    scheduler.add_job('email', send_simple_email, args=[current_app._get_current_object()])
-
-def job_1():
-    print('123')
-
 # official working function 
 def add_reminder(app):
     print(scheduler.get_job("reminder"))
